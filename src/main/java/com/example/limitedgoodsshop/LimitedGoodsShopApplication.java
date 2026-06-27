b48582b7ac7e1799d00b2d4180b6ccdf7c23bd0e
Jinuk-Heo <hju1650@gmail.com>
2026-06-28 08:44:45 +0900

    첫 번째 코드 올리기

---
+++ src/main/java/com/example/limitedgoodsshop/LimitedGoodsShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.limitedgoodsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedGoodsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitedGoodsShopApplication.class, args);
+    }
+
+}
